a6a2be6b52bf2d56b023ffc8d1a811ab6b66239e
Akshay Abraham <AkshayAbraham@users.noreply.huggingface.co>
2025-05-26 12:50:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -536,6 +536,10 @@ button[aria-label="Scroll to bottom"] {
     height: 150px;
     margin-bottom: 15px;
 }
+#suggestion_submit_btn_gradio {
+    background-color: #2ECC71 !important; /* New green color */
+    border-radius: 8px !important;
+}
 """
 
 # --- Gradio UI ---
@@ -561,8 +565,8 @@ with gr.Blocks(css=custom_css) as demo:
             gr.HTML("""
             <div class="contact-info">
                 <p>Feel free to reach out directly or connect on my social channels:</p>
-                <p><strong>Email:</strong> <a href="mailto:akshay.abraham@example.com">akshay.abraham@example.com</a></p>
-                <p><strong>Phone:</strong> +44 123 456 7890</p>
+                <p><strong>Email:</strong> <a href="mailto:akshayabraham542@.com">akshayabraham542@gmail.com</a></p>
+    
             </div>
             <div class="social-links">
                 <a href="https://www.linkedin.com/in/akshay-abraham/" target="_blank" title="LinkedIn">
@@ -570,7 +574,7 @@ with gr.Blocks(css=custom_css) as demo:
                         <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-.984-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
                     </svg>
                 </a>
-                <a href="https://github.com/akshay-abraham" target="_blank" title="GitHub">
+                <a href="https://github.com/AkshayAbraham" target="_blank" title="GitHub">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.087-.731.084-.716.084-.716 1.205.082 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.493.998.108-.776.417-1.305.76-1.605-2.665-.304-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.118-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576c4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
                     </svg>
